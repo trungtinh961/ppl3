225e9324f3b7a58d4d81899f99b40966db3c47ce
trungtinh961 <you@example.com>
2019-11-11 21:23:10 +0700

    1

--- initial/src/main/mc/checker/StaticCheck.py
+++ initial/src/main/mc/checker/StaticCheck.py
@@ -6,6 +6,7 @@ from AST import *
 from Visitor import *
 from Utils import Utils
 from StaticError import *
+from functools import reduce
 
 class MType:
     def __init__(self,partype,rettype):
@@ -20,45 +21,85 @@ class Symbol:
 
 class StaticChecker(BaseVisitor,Utils):
 
-    global_envi = [
-    Symbol("getInt",MType([],IntType())),
-    Symbol("putIntLn",MType([IntType()],VoidType()))
+    global_envi = [    
+        Symbol("getInt",MType([],IntType())),
+        Symbol("putInt",MType([IntType()],VoidType())),
+        Symbol("putIntLn",MType([IntType()],VoidType())),
+        Symbol("getFloat",MType([],FloatType())),
+        Symbol("putFloat",MType([FloatType()],VoidType())),
+        Symbol("putFloatLn",MType([FloatType()],VoidType())),
+        Symbol("putBool",MType([BoolType()],VoidType())),
+        Symbol("putBoolLn",MType([BoolType()],VoidType())),
+        Symbol("putString",MType([StringType()],VoidType())),
+        Symbol("putStringLn",MType([StringType()],VoidType())),
+        Symbol("putLn",MType([],VoidType()))
     ]
             
-    
     def __init__(self,ast):
         #print(ast)
         #print(ast)
         #print()
-        self.ast = ast
+        self.ast = ast 
 
- 
-    
+    def flatten(self,lst):
+        return [item for sublist in lst for item in sublist]
+
     def check(self):
         return self.visit(self.ast,StaticChecker.global_envi)
 
-    def visitProgram(self,ast, c): 
-        return [self.visit(x,c) for x in ast.decl]
+    def checkRedeclared(self,symbol,kind,env):
+        if self.lookup(symbol.name, env, lambda x:x.name):
+            raise Redeclared(kind,symbol.name)
+        else:
+            return [symbol]
+
+    def getName(self, decl):
+        if isinstance(decl, VarDecl):
+            return decl.variable
+        else:
+            return decl.name.name
+    
+    def getType(self, decl):
+        if isinstance(decl, VarDecl):
+            return decl.varType
+        else:
+            return MType([z.varType for z in decl.param], decl.returnType)
+
+    def visitProgram(self,ast,c):
+        lstDecl = []
+        for x in ast.decl:
+            lstDecl.append(Symbol(self.getName(x),self.getType(x)))
+        lstFunc = []
+        lstFunc = filter(lambda x: isinstance(x.mtype,MType),lstDecl)
+        
+        if self.lookup("main", lstFunc, lambda x: x.name) is None:
+            raise NoEntryPoint()
+
+        return reduce(lambda x,y: x + self.visit(y,[x+c,lstDecl+c]),ast.decl,[])
+
+    def visitVarDecl(self,ast,c):
+        return self.checkRedeclared(Symbol(self.getName(ast), self.getType(ast)),Variable(),c[0])
+        
 
     def visitFuncDecl(self,ast, c): 
-        return list(map(lambda x: self.visit(x,(c,True)),ast.body.member)) 
-    
+         return self.checkRedeclared(Symbol(ast.name.name,MType([x.varType for x in ast.param],ast.returnType)),Function(),c[0])
 
-    def visitCallExpr(self, ast, c): 
-        at = [self.visit(x,(c[0],False)) for x in ast.param]
+   
+    # def visitCallExpr(self, ast, c): 
+    #     at = [self.visit(x,(c[0],False)) for x in ast.param]
         
-        res = self.lookup(ast.method.name,c[0],lambda x: x.name)
-        if res is None or not type(res.mtype) is MType:
-            raise Undeclared(Function(),ast.method.name)
-        elif len(res.mtype.partype) != len(at):
-            if c[1]:
-                raise TypeMismatchInStatement(ast)
-            else:
-                raise TypeMismatchInExpression(ast)
-        else:
-            return res.mtype.rettype
+    #     res = self.lookup(ast.method.name,c[0],lambda x: x.name)
+    #     if res is None or not type(res.mtype) is MType:
+    #         raise Undeclared(Function(),ast.method.name)
+    #     elif len(res.mtype.partype) != len(at):
+    #         if c[1]:
+    #             raise TypeMismatchInStatement(ast)
+    #         else:
+    #             raise TypeMismatchInExpression(ast)
+    #     else:
+    #         return res.mtype.rettype
 
-    def visitIntLiteral(self,ast, c): 
-        return IntType()
+    # def visitIntLiteral(self,ast, c): 
+    #     return IntType()
     
 
--- initial/src/test/ASTGenSuite.py
+++ initial/src/test/ASTGenSuite.py
@@ -25,3 +25,28 @@ class ASTGenSuite(unittest.TestCase):
         expect = str(Program([FuncDecl(Id("main"),[],IntType(),Block([CallExpr(Id("getIntLn"),[])]))]))
         self.assertTrue(TestAST.checkASTGen(input,expect,302))
    
+   def test_all(self):        
+        input = """
+        void main()
+        {
+            int n;
+            float a, b, c ;
+            float R;
+            float P;
+            float S;
+            do
+            {
+                printf("Nhap ban kinh duong tron:");
+                scanf("%f", R);
+            }
+            while(R <= 0);
+            P = 2 * PI * R;
+            S = PI * R * R;
+            printf("Chu vi hinh tron : %f dvdd", P);
+            printf("Dien tich hinh tron : %f dvdt", S);
+            break;               
+            getch();
+        }
+        """
+        expect = str(Program([FuncDecl(Id("main"),[],VoidType(),Block([VarDecl("n",IntType()),VarDecl("a",FloatType()),VarDecl("b",FloatType()),VarDecl("c",FloatType()),VarDecl("R",FloatType()),VarDecl("P",FloatType()),VarDecl("S",FloatType()),Dowhile([Block([CallExpr(Id("printf"),[StringLiteral("Nhap ban kinh duong tron:")]),CallExpr(Id("scanf"),[StringLiteral("%f"),Id("R")])])],BinaryOp("<=",Id("R"),IntLiteral(0))),BinaryOp("=",Id("P"),BinaryOp("*",BinaryOp("*",IntLiteral(2),Id("PI")),Id("R"))),BinaryOp("=",Id("S"),BinaryOp("*",BinaryOp("*",Id("PI"),Id("R")),Id("R"))),CallExpr(Id("printf"),[StringLiteral("Chu vi hinh tron : %f dvdd"),Id("P")]),CallExpr(Id("printf"),[StringLiteral("Dien tich hinh tron : %f dvdt"),Id("S")]),Break(),CallExpr(Id("getch"),[])]))]))
+        self.assertTrue(TestAST.checkASTGen(input,expect,303))
